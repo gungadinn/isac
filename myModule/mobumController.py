dae593b371b33564441f73a5193a97e1d84622e5
gungadinn <ch-aa@naver.com>
2019-09-12 22:09:46 +0900

    add ISAC files

---
+++ myModule/mobumController.py
@@ -0,0 +1,21 @@
+# 모범사례 보여주기
+from flask import request
+from myModule import counselingdao
+from myModule.usadoModel import doc2vec_model
+import numpy
+
+class MobumController():
+    def __init__(self):
+        pass
+
+    def getMobum(self):
+        title = request.form.get('inputTitle')
+        result1,result2vec, finalResult = doc2vec_model.doc2vec_weight_comp(title)
+        return result1,result2vec, finalResult
+
+    def getMobumContent(self, counselID):
+        mobumContent = counselingdao.getMobumCounsel(counselID)
+        return mobumContent
+
+
+mobumController = MobumController()
